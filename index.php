31370e7f8e0538c52b97a707427a6a056ab4b838
ZiXian92 <zixian1992@hotmail.com>
2014-05-21 23:27:01 +0800

    Implements basic view and control components of MVC framework

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+	/* The controller component of the MVC framework.
+	 * Currently handles only page requests.
+	 * Form submission requests to be handled later upon figuring out the
+	 * SQL portion of the model component.
+	 * More edits to be made upon successful URL rewriting for friendlier
+	 * URLs.
+	 */
+
 	include 'view.php';
 
 	if(!isset($_GET['page']))
--- view.php
+++ view.php
@@ -1,9 +1,19 @@
 <?php
+	/* Defines the view component of the MVC framework.
+	 * Defining view as a class object causes many statements to be
+	 * not executed so this function is the next better alternative.
+	 * Currently handles only page requests.
+	 * To be modified when more placeholders in the template are needed.
+	 * Note: As this function reads in the entire template as a big chunk
+	 * of text, network bandwidth may be insufficient if template.html
+	 * gets too big.
+	 * Comments by Zi Xian
+	 */
 	function render_page($page){
 		$content = file_get_contents("template.html");
 		$content = str_replace("{{title}}", $page, $content);
 		$content = str_replace("{{content}}", file_get_contents($page.".html"), $content);
-		if(page==='create_entry')
+		if($page=='create_entry')
 			$content = str_replace("{{javascript}}", "<script src=\"javascripts/jscript.js\"></script>", $content);
 		else
 			$content = str_replace("{{javascript}}", "", $content);
