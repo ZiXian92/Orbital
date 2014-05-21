8c96f3261e349c77c2e0f7ab09f4122b9cebc490
ZiXian92 <zixian1992@hotmail.com>
2014-05-21 16:44:54 +0800

    Splits template and contents

--- index.php
+++ index.php
@@ -1,34 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Home-XXX</title>
-	<link rel="stylesheet" type="text/css" href="stylesheets/style.css">
-</head>
+<?php
 
-<body>
-	<!--Imports the header and navigation links-->
-	<?php 
-	include('includes/header.php');
-	include('includes/nav.php');
-	?>
-
-	<!--The main content of the page-->
-	<div id="content">
-		<p>Do you have photos of any significant life events that you
-		do not wish to forget? Or do you want to share these life events
-		with your friends? Then you have come to the right place! XXX
-		will help you get it done within a few minutes.
-		</p>
-
-		<h2>New to this site?</h2>
-		<!--Insert image button link to guided process-->
-
-		<h2>I've done this before</h2>
-		<!--Insert image button link to the standard form-->
-		<p style="text-align: center;"><a href="create_entry.php">Let's Get Started!</a></p>
-	</div>
-
-	<!--Imports the footer-->
-	<?php include('includes/footer.php'); ?>
-</body>
-</html>
+?>
