85992bfb7efb3813e115858f5ffcd1fecec9a4cb
ZiXian92 <zixian1992@hotmail.com>
2014-05-22 11:45:14 +0800

    Defines a View class in view2.php

---
+++ view2.php
@@ -0,0 +1,29 @@
+<?php
+	class View{
+		private $_template = "template.html";
+		private $_content;
+		public function __construct($arr){
+			$this->_content = $this->get_template();
+			$this->setContent($arr);
+		}
+		private function get_template(){
+			return file_get_contents($this->_template);
+		}
+		private function setContent($arr){
+			foreach($arr as $key=>$value)
+				$this->_content = str_replace("{{".$key."}}",
+					$value, $this->_content);
+		}
+		public function render(){
+			echo $this->_content;
+		}
+	}
+	
+	$dict = array();
+	$dict['title'] = 'Home';
+	$dict['javascript'] = '';
+	$dict['content'] = file_get_contents('home.html');
+	$view = new View($dict);
+	$view->render();
+	#print_r($dict);
+?>
