40a4f08c984351b1bbcc881cf1b8544da1ddd87a
ZiXian92 <zixian1992@hotmail.com>
2014-05-24 00:03:06 +0800

    Adds simple file upload to server directory functionaltiy

--- javascripts/jscript.js
+++ javascripts/jscript.js
@@ -1,11 +1,22 @@
 //Returns true if ALL fields are not empty and false otherwise
 function validate(){
-	var a = document.forms["new_entry"]["img_file"].value;
-	var b = document.forms["new_entry"]["title"].value;
-	var c = document.forms["new_entry"]["story"].value;
-	if(a=="" || b=="" || c==""){
+	var filename = document.forms["new_entry"]["img_file"].value;
+	var title = document.forms["new_entry"]["title"].value;
+	var text = document.forms["new_entry"]["story"].value;
+	if(filename=="" || title=="" || text==""){
 		alert("All fields are required.");
 		return false;
 	}
 	return true;
+}
+
+function validate_file(){
+	var filename = document.forms["new_entry"]["img_file"].value;
+	var arr = str.split(filename, ".");
+	var ext = arr[arr.length-1];
+	alert(ext);
+	if(ext!="jpg" && ext!="jpeg" && ext!="png"){
+		document.forms["new_entry"]["img_file"]="";
+		alert("Please upload an image file(extensions: jpg, jpeg, png");
+	}
 }
--- upload_handler.php
+++ upload_handler.php
@@ -1,5 +1,20 @@
 <?php
+/* This file only handles form submissions.
+ * To be incorporated or called by index.php
+ * Temporary measure, only to test working functionality.
+ * Next step is to write the image and all form details preoperly into
+ * PDF file
+ * Commented by: Qua Zi Xian
+ */
 #This file only handles form submissions.
-	if($_SERVER['REQUEST_METHOD']=='POST')
-		echo("Submitted");
+	if($_SERVER['REQUEST_METHOD']=="POST"){
+		echo "File name: ".$_FILES['img']['name']."<br/>";
+		echo "Title: ".$_POST['title']."<br/>";
+		echo "Story: ".$_POST['story'];
+
+	/* Destination folder requires permission setting of 777 instead of
+	 * 755 or 766. Why?
+	 */
+		move_uploaded_file($_FILES['img']['tmp_name'], "../uploads/{$_FILES['img']['name']}");
+	}
 ?>
