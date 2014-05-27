843b852a6feaf93bdf287ba28b903fe91807d839
ZiXian92 <zixian1992@hotmail.com>
2014-05-27 12:49:08 +0800

    Cleans up upload_handler.php

--- upload_handler.php
+++ upload_handler.php
@@ -6,28 +6,21 @@
 	/* FPDF class definition is in the specified file */
 	require 'PDF.php';
 
-	/*if($_SERVER['REQUEST_METHOD']!="POST"){
-		//Redirect to create_entry page
-	}*/
-
-	/* Moves image to uploads folder in server for use in PDF
-	 * Image to be deleted form uploads folder after use
+	if($_SERVER['REQUEST_METHOD']=="POST"){
+	/* Moves image to uploads folder in server for use in PDF.
 	 * Please create the destination folder called uploads with the same
 	 * relative paths in the 2nd parameter of move_uploaded_file.
 	 * Destination folder requires permission setting of 777 instead of
 	 * 755 or 766. Why?
+	 * To-Do: Remove image from uploads directory after use.
 	 */
-	move_uploaded_file($_FILES['img']['tmp_name'], "../uploads/{$_FILES['img']['name']}");
-	
+		move_uploaded_file($_FILES['img']['tmp_name'], "../uploads/{$_FILES['img']['name']}");
 
-	/* Sends the document to the specified destination.
-	 * 1st parameter is the name to be given to document.
-	 * Options: I = Show in browser, with download option
-	 *	    D = Send to browser and force download
-	 *	    F = Save to local file
-	 *	    S = Return document as string
-	 * Change the destination to D before publishing.
-	 */
-	$pdf = new PDF();
-	unset($pdf);
+		/* Creates and loads form contents into a new PDF document */
+		$pdf = new PDF();
+		unset($pdf);
+	}
+	else{
+		//Redirect to form entry page
+	}
 ?>
