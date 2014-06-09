08fa2f83f64e2872b791660bb6108c125c3d982c
ZiXian92 <zixian1992@hotmail.com>
2014-06-09 16:00:13 +0800

    Completes viewing and deletion of entries functionalities

--- entries_handler.php
+++ entries_handler.php
@@ -14,17 +14,29 @@
 		exit(0);
 	}
 
+	/* On passing the logged in check, authenticates if the user is
+	 * dealing with his/her own entry and not someone else's.
+	 */
 	$model = new Model();
 
 	if($model->authenticate_entry_request($_SESSION['user_id'], $_GET['id'])){
+		/* Gets the file path since the file will be
+		 * dealt with anyway.
+		 */
 		$file = $model->get_entry_file($_GET['id']);
+
+		/* Applies the appropriate action based on the request */
 		switch($_GET['action']){
-			case "view": break;
+			case "view": header("Content-type: application/pdf");
+					header("Content-disposition: inline; filename=\"entry.pdf\"");
+					readfile($file);
+					exit(0);
 			case "delete": unlink($file);
 					$model->remove_entry($_GET['id']);
 					break;
 		}
 	}
 
+	/* Redirects to home page */
 	header("Location: http://".$_SERVER['HTTP_HOST']);
 ?>
