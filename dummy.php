5a6822c0e7a563b2751942ccf6c708874311965d
ZiXian92 <zixian1992@hotmail.com>
2014-07-11 13:20:41 +0800

    Runs test code to make calls to Dropbox API

--- dummy.php
+++ dummy.php
@@ -3,17 +3,8 @@
 	use \Dropbox as dbx;
 	#mail("zixian1992@hotmail.com", "Hello", "Mizuki Nana desu~", "From: admin@relivethemoment.host-ed.me\r\n");
 
-	function getWebAuth(){
-		$appInfo = dbx\AppInfo::loadFromJsonFile("app-info.json");
-		$clientIdentifier = "relivethatmoment/1.0";
-		$redirectUri = "https://relivethatmoment.herokuapp.com/dropbox-auth-finish";
-		$csrfTokenStore = new dbx\ArrayEntryStore($_SESSION, 'dropbox-auth-csrf-token');
-		return new dbx\WebAuth($appInfo, $clientIdentifier, $redirectUri, $csrfTokenStore);
-	}
-
-	$authorizeUrl = getWebAuth()->start();
-	header("Location: ".$authorizeUrl);
-
-	list($accessToken, $userId) = getWebAuth()->finish($_GET);
-	file_put_contents("accessToken.txt", $accessToken);
+	$accessToken = file_get_contents("accessToken.txt");
+	$dbxClient = new dbx\Client($accessToken, "relivethatmoment/1.0")
+	$accountInfo = $dbxClient->getAccountInfo();
+	echo $accountInfo;
 ?>
