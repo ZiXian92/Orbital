234c7a6abfd56a852d14bb85b12a933674d61a38
ZiXian92 <zixian1992@hotmail.com>
2014-07-11 17:54:45 +0800

    Modifies code for viewing of entries

--- entries_handler.php
+++ entries_handler.php
@@ -2,6 +2,8 @@
 	/* Handles entry view and delete requests */
 
 	require 'model.php';
+	require_once 'dropbox-sdk/Dropbox/autoload.php';
+	use \Dropbox as dbx;
 
 	session_start();
 	
@@ -25,12 +27,27 @@
 		 */
 		$file = $model->get_entry_file($_GET['id']);
 
+	/* Loads Dropbox access token */
+		$accessToken = file_get_contents("accessToken.txt");
+
+	/* Creates a new Dropbox client to access API */
+		$dbxClient = new dbx\Client($accessToken, "relivethatmoment/1.0");
+
 		/* Applies the appropriate action based on the request */
 		switch($_GET['action']){
 			case "view": header("Content-type: application/pdf");
-					header("Content-disposition: inline; filename=\"entry.pdf\"");
-					readfile($file);
+				header("Content-disposition: inline; filename=\"entry.pdf\"");
+				$f = fopen("/tmp/".$file, "wb");
+
+		/* Displays the file if it exists in the storage */
+				if($dbxClient->getFile("/".$file, $f)!=null){
+					readfile("/tmp/".$file);
+					fclose($f);
+					unlink("/tmp/".$file);
 					exit(0);
+				}
+				file_put_contents("message.txt", "File does not exist.");
+				break;
 			case "delete": unlink($file);
 					$model->remove_entry($_GET['id']);
 					break;
