ae1d04ac7509b4eb2193ecb27954d8a827b26763
ZiXian92 <zixian1992@hotmail.com>
2014-07-13 16:20:18 +0800

    Adds experimental code to send email to my email(Please DO NOT SPAM)

--- dummy.php
+++ dummy.php
@@ -1,29 +1,37 @@
 <?php
-	require_once "dropbox-sdk/Dropbox/autoload.php";
-	use \Dropbox as dbx;
-	#mail("zixian1992@hotmail.com", "Hello", "Mizuki Nana desu~", "From: admin@relivethemoment.host-ed.me\r\n");
+	$url = 'https://api.sendgrid.com/';
+	$user = 'zixian';
+	$pass = 'Nana7Nana';
 
-	/*function getWebAuth(){
-		$appInfo = dbx\AppInfo::loadFromJsonFile("app-info.json");
-		$clientIdentifier = "localhost/1.0";
-		$redirectUri = "https://localhost/dropbox-auth-finish";
-		$csrfTokenStore = new dbx\ArrayEntryStore($_SESSION, 'dropbox-auth-csrf-token');
-		return new dbx\WebAuth($appInfo, $clientIdentifier, $redirectUri, $csrfTokenStore);
-	}*/
+	$params = array(
+		'api_user' => $user,
+		'api_key' => $pass,
+		'to' => 'zixian1992@hotmail.com',
+		'subject' => 'First SendGrid Email',
+		'text' => 'This is the first email successfully sent using SendGrid!',
+		'from' => 'zixian1992@hotmail.com',
+	);
 
-	$accessToken = file_get_contents("accessToken.txt");
-	$dbxClient = new dbx\Client($accessToken, "relivethatmoment/1.0");
-	#$accountInfo = $dbxClient->getAccountInfo();
-	#print_r($accountInfo);
-	#$f = fopen("blank.txt", "r");
-	#$res = $dbxClient->uploadFile("/blank.txt", dbx\WriteMode::add(), $f);
-	#fclose($f);
-	try{
-		$res = $dbxClient->delete("/blank.txt");
-		print_r($res);
-	}
-	catch(Exception $e){}
-	finally{
-		echo "File no longer exists.";
-	}
+	$request = $url."api/mail.send.json";
+
+	#Create new cURL request
+	$session = curl_init($request);
+
+	#Use HTTP POST
+	curl_setopt($session, CURLOPT_POST, true);
+
+	#Sets the POST parameters
+	curl_setopt($session, CURLOPT_POSTFIELDS, $params);
+
+	#Don't return headers, but returns the response
+	curl_setopt($session, CURLOPT_HEADER, false);
+	curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+	#Execute the request and get response
+	$response = curl_exec($session);
+
+	#Closes the session
+	curl_close($session);
+
+	print_r($response);
 ?>
