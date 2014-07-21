41e7341f7b0f3e23568ea674dab642f0ffdb0579
ZiXian92 <zixian1992@hotmail.com>
2014-07-21 14:27:57 +0800

    Updates list_users function in model.php

--- model.php
+++ model.php
@@ -222,9 +222,9 @@
 					if($row['activated'])
 						$list.="<td>Activated</td>";
 					else
-						$list.="<td><a href=\"users.php?action=activate&id=".$row['id']."\">Activate</a></td>";
-					$list.="<td><a href=\"users.php?action=reset_passwd&name=".$row['username']."&email=".urlencode($row['email'])."\">Reset Password</a></td>
-					<td><a href=\"admin.php?action=delete&id=".(string)$row['id']."\" onclick=\"return confirm_delete();\">Delete</a></td></tr>";
+						$list.="<td id=\"active_".$id."\"><a href=\"users/activate/".$id."\" onclick=\"activate(event, ".$id.");\">Activate</a></td>";
+					$list.="<td><a href=\"users/reset_password/".$name."/".urlencode($email)."\" onclick=\"admin_reset_password(event, '".$name."', '".$email."');\">Reset Password</a></td>
+					<td><a href=\"admin/delete/".(string)$id."\" onclick=\"delete_user(event, ".(string)$id.");\">Delete</a></td></tr>";
 
 		/* Every 10th user is the last of the group of 10 */
 				if($counter%10==0)
