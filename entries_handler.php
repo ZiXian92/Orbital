e7ce986175f7fd7f64df6a7ac72c56d05cbe989e
ZiXian92 <zixian1992@hotmail.com>
2014-07-21 18:08:11 +0800

    Cleans up old code in entries_handler.php

--- entries_handler.php
+++ entries_handler.php
@@ -104,48 +104,4 @@
 			header('Location: https://'.$_SERVER['HTTP_HOST'].'/404');
 	}
 	exit();
-
-	/* On passing the logged in check, authenticates if the user is
-	 * dealing with his/her own entry and not someone else's.
-	 */
-	$model = new Model();
-
-	if($model->authenticate_entry_request($_SESSION['user_id'], $_GET['id'])){
-		/* Gets the access token to access Dropbox API */
-		$accessToken = file_get_contents("accessToken.txt");
-
-		/* Creates Dropbox client to access files */
-		$dbxClient = new dbx\Client($accessToken, "relivethatmoment/1.0");
-		/* Gets the file path since the file will be
-		 * dealt with anyway.
-		 */
-		$file = $model->get_entry_file($_GET['id']);
-
-		/* Applies the appropriate action based on the request */
-		switch($_GET['action']){
-			case "view": header("Content-type: application/pdf");
-				header("Content-disposition: inline; filename=\"entry.pdf\"");
-				/* Downloads the PDF file into /tmp folder */
-				$f = fopen("/tmp/".$file, "wb");
-				if($dbxClient->getFile("/".$file, $f)!=null){
-					readfile("/tmp/".$file);
-					fclose($f);
-					unlink("/tmp/".$file);
-					exit(0);
-				}
-				file_put_contents("message.txt", "Entry does not exist. Please delete t from the records.");
-				#readfile($file);
-				break;
-			case "delete": #unlink($file);
-				try{
-					$dbxClient->delete("/".$file);
-				}
-				catch(Exception $e){}
-				$model->remove_entry($_GET['id']);
-				break;
-		}
-	}
-
-	/* Redirects to home page */
-	header("Location: https://".$_SERVER['HTTP_HOST']);
 ?>
