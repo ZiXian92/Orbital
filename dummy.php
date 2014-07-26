1055515fd031e91e02c77d3ac511899af868a349
ZiXian92 <zixian1992@hotmail.com>
2014-07-26 19:37:38 +0800

    Almost complete Facebook test code in dummy.php

--- dummy.php
+++ dummy.php
@@ -1,5 +1,7 @@
 <?php
 	require 'facebook-php-sdk-v4-4.0-dev/autoload.php';
+	use Facebook\FacebookSession;
+	use Facebook\FacebookJavaScriptLoginHelper;
 	use Facebook\FacebookRequest;
 	use Facebook\GraphUser;
 	use Facebook\FacebookRequestException;
@@ -15,17 +17,24 @@
 		}
 	}
 
+	$arr = json_decode(file_get_contents('fbsdk.json'), true);
+
+	FacebookSession::setDefaultApplication($arr['app_id'], $arr['app_secret']);
+
 	$fbsess = createFBSession();
 	if($fbsess){
 		try{
 			$user_profile = (new FacebookRequest($fbsess, 'GET', '/me'))->execute()->getGraphObject();
 			$name = $user_profile->getProperty('name');
 			$email = $user_profile->getProperty('email');
-			$model = new Model();
+			echo $name."<br/>".$email;
+			#$model = new Model();
 			#if !user exists
-				$model->add_user($model->get_user_id, $name, null, $email, null);
-			$user = $model->get_user($email, null);
+				#$model->add_user($model->get_user_id, $name, null, $email, null);
+			#$user = $model->get_user($email, null);
 				
 		} catch(Exception $e){}
 	}
+	else
+		echo 'Not logged in to Facebook.';
 ?>
