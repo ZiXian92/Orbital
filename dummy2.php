a9bf120d1e5ac01b50640d9041ecd69ac945d4d8
ZiXian92 <zixian1992@hotmail.com>
2014-07-27 22:17:04 +0800

    Attempts to create session in dummy2.php

--- dummy2.php
+++ dummy2.php
@@ -1,5 +1,8 @@
 <?php
 	require 'Facebook/FacebookSession.php';
+	require 'Facebook/FacebookJavaScriptLoginHelper.php';
 	FacebookSession::setDefaultApplication("823148504363911", "2e6875c6ad028b5a7f1099016b4f535f");
+	$helper = new FacebookJavaScriptLoginHelper();
+	var_dump($helper);
 	echo 'Hello';
 ?>
