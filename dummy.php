40d32c6c5f5d77029784fe91104f252171475e98
ZiXian92 <zixian1992@hotmail.com>
2014-07-27 13:04:46 +0800

    Adds sample code to dummy.php

--- dummy.php
+++ dummy.php
@@ -1,5 +1,7 @@
 <?php
 	require 'facebook-php-sdk-v4-4.0-dev/autoload.php';
+	use Facebook\FacebookSession;
+	use Facebook\FacebookJavaScriptLoginHelper;
 	use Facebook\FacebookRequest;
 	use Facebook\GraphUser;
 	use Facebook\FacebookRequestException;
@@ -14,17 +16,19 @@
 			return null;
 		}
 	}
-
+	$app_info = json_decode(file_get_contents('fbsdk.json'), true);
+	FacebookSession::setDefaultApplication();
 	$fbsess = createFBSession();
 	if($fbsess){
 		try{
 			$user_profile = (new FacebookRequest($fbsess, 'GET', '/me'))->execute()->getGraphObject();
 			$name = $user_profile->getProperty('name');
 			$email = $user_profile->getProperty('email');
-			$model = new Model();
+			echo $name.'<br/>'.$email;
+			#$model = new Model();
 			#if !user exists
-				$model->add_user($model->get_user_id, $name, null, $email, null);
-			$user = $model->get_user($email, null);
+				#$model->add_user($model->get_user_id, $name, null, $email, null);
+			#$user = $model->get_user($email, null);
 				
 		} catch(Exception $e){}
 	}
