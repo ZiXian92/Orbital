663a45202570f64ec2f0fa7ee45f08652b677ba5
ZiXian92 <zixian1992@hotmail.com>
2014-07-27 22:03:44 +0800

    Starts tracing problem in dummy.php

--- dummy.php
+++ dummy.php
@@ -24,6 +24,7 @@
 	}
 	ini_set('display_errors', 'On');
 
+	echo 'Hello';
 	FacebookSession::setDefaultApplication("823148504363911", "2e6875c6ad028b5a7f1099016b4f535f");
 
 	$fbsess = createFBSession();
