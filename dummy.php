5a2fc8365fb6385815ca868a7677c36e9b3b6aee
ZiXian92 <zixian1992@hotmail.com>
2014-07-28 12:44:07 +0800

    Adds test code to dummy.php

--- dummy.php
+++ dummy.php
@@ -1,49 +1,6 @@
 <?php
-	require 'facebook-php-sdk-v4-4.0-dev/autoload.php';
-	require 'facebook-php-sdk-v4-4.0-dev/src/Facebook/Entities/AccessToken.php';
-	require 'facebook-php-sdk-v4-4.0-dev/src/Facebook/Entities/SignedRequest.php';
-	use Facebook\FacebookSession;
-	use Facebook\FacebookRedirectLoginHelper;
-	use Facebook\FacebookJavaScriptLoginHelper;
-	use Facebook\FacebookRequest;
-	use Facebook\GraphUser;
-	use Facebook\FacebookRequestException;
+	require 'model.php';
 
-	#Returns a new Facebook session if a user is logged in.
-	#Returns null otherwise.
-	function createFBSession(){
-		$helper = new FacebookRedirectLoginHelper('http://relivethatmoment.herokuapp.com/dummy.php');
-		var_dump($helper);
-		echo '<br/>';
-		try{
-			var_dump($helper->getSessionFromRedirect());
-			echo '<br/>';
-			return $helper->getSessionFromRedirect();
-		} catch(Exception $e){
-			echo 'Exception caught.<br/>';
-			return null;
-		}
-	}
-	ini_set('display_errors', 'On');
-	$arr = json_decode(file_get_contents('fbsdk.json'), true);
-
-	FacebookSession::setDefaultApplication($arr['app_id'], $arr['app_secret']);
-
-	$fbsess = createFBSession();
-	#var_dump($fbsess);
-	if($fbsess){
-		try{
-			$user_profile = (new FacebookRequest($fbsess, 'GET', '/me'))->execute()->getGraphObject();
-			$name = $user_profile->getProperty('name');
-			$email = $user_profile->getProperty('email');
-			echo $name."<br/>".$email;
-			#$model = new Model();
-			#if !user exists
-				#$model->add_user($model->get_user_id, $name, null, $email, null);
-			#$user = $model->get_user($email, null);
-				
-		} catch(Exception $e){}
-	}
-	else
-		echo 'Not logged in to Facebook.';
+	$model = new Model();
+	echo $model->contains_user('zixian', 'zx@email.com');
 ?>
--- dummy2.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require 'Facebook/FacebookSession.php';
-	require 'Facebook/FacebookJavaScriptLoginHelper.php';
-	FacebookSession::setDefaultApplication("823148504363911", "2e6875c6ad028b5a7f1099016b4f535f");
-	$helper = new FacebookJavaScriptLoginHelper();
-	var_dump($helper);
-	echo 'Hello';
-?>
