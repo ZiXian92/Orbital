0a40ca72e2e4fb5ace1377ee6223a5ec93a2b23a
ZiXian92 <zixian1992@hotmail.com>
2014-08-04 22:16:06 +0800

    Attempts to fix change_password in users.php

--- users.php
+++ users.php
@@ -279,7 +279,7 @@
 	#Changes a user's password
 	function changepassword(){
 		try{
-			if($_SERVER['REQUEST_METHOD']=='POST' && $_SERVER['CONTENT_TYPE']=='application/json; charset=UTF-8'){
+			if($_SERVER['REQUEST_METHOD']=='POST' && preg_match('/^application\/json/', $_SERVER['CONTENT_TYPE'])){
 				$req_params = json_decode(file_get_contents('php://input'), true);
 				$oldpass = strip_tags($req_params['oldpass']);
 				$newpass = strip_tags($req_params['newpass']);
