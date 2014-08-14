fa4e89b74a88f6b4fe06fcd71076d42dd61d2343
ZiXian92 <zixian1992@hotmail.com>
2014-08-14 17:33:32 +0800

    Cleans up code in jscript.js

--- javascripts/jscript.js
+++ javascripts/jscript.js
@@ -80,41 +80,14 @@ function validate_entry(ev){
 	if(filename=="" || title=="" || text=="" || checkFile!='Ok')
 		document.getElementById('error').innerHTML = '<div class="alert alert-danger" role="alert">Please make sure all fields are properly filled</div>';
 	else{
-      		/*window.fbAsyncInit = function() {
-			FB.init({
-				appId      : '595408083913803',
-				cookie     : true,  // enable cookies to allow the server to access the session
-				xfbml      : true,  // parse social plugins on this page
-				version    : 'v2.0' // use version 2.0
-			});
-
-			FB.getLoginStatus(function(response){
-				if(response.status==='connected'){
-					FB.login(function(){
-						FB.api('/me/feed', 'post', {message: title+'\n'+text});
-					}, {scope: 'publish_actions'});
-				}
-				//document.forms[0].submit();
-			});
-		};*/
-
 		FB.getLoginStatus(function(response){
 			if(response.status==='connected'){
 				FB.login(function(){
 					FB.api('/me/feed', 'post', {message: title+'\n'+text});
 				}, {scope: 'publish_actions'});
 			}
-			//document.forms[0].submit();
+			document.forms[0].submit();
 		});
-
-		// Load the SDK asynchronously
-		/*(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) return;
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/en_US/sdk.js";
-		fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));*/
 	}
 }
 
